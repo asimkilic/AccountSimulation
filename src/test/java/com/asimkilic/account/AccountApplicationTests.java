bafcba9fea50388a87e7d73d2e1cfcc4d89f7b03
asimkilic <a.asim.kilic@gmail.com>
2022-01-03 19:12:36 +0300

    packages created

---
+++ src/test/java/com/asimkilic/account/AccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asimkilic.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
