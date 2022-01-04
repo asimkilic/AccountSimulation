866ea013fbc69337435186d29fe9c899ecc0ef02
asimkilic <a.asim.kilic@gmail.com>
2022-01-04 17:11:37 +0300

    Transaction services deleted

--- src/main/java/com/asimkilic/account/AccountApplication.java
+++ src/main/java/com/asimkilic/account/AccountApplication.java
@@ -1,6 +1,8 @@
 package com.asimkilic.account;
 
 import com.asimkilic.account.model.Account;
+import com.asimkilic.account.model.Customer;
+import com.asimkilic.account.repository.CustomerRepository;
 import kotlin.collections.SetsKt;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -8,16 +10,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.HashSet;
 
 @SpringBootApplication
 public class AccountApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountApplication.class, args);
-	}
+    private final CustomerRepository customerRepository;
 
-	@Override
-	public void run(String... args) throws Exception {
-		Account a = new Account("a", BigDecimal.ONE, LocalDateTime.now(),null, SetsKt.emptySet());
-	}
+    public AccountApplication(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Customer customer = customerRepository.save(new Customer("", "Asım", "KILIÇ", new HashSet<>()));
+        System.out.println(customer + " ------ id: " + customer.getId());
+    }
 }
--- src/main/java/com/asimkilic/account/service/AccountService.java
+++ src/main/java/com/asimkilic/account/service/AccountService.java
@@ -17,16 +17,13 @@ public class AccountService {
 
     private final AccountRepository accountRepository;
     private final CustomerService customerService;
-    private final TransactionService transactionService;
     private final AccountDtoConverter accountDtoConverter;
 
     public AccountService(AccountRepository accountRepository,
                           CustomerService customerService,
-                          TransactionService transactionService,
                           AccountDtoConverter accountDtoConverter) {
         this.accountRepository = accountRepository;
         this.customerService = customerService;
-        this.transactionService = transactionService;
         this.accountDtoConverter = accountDtoConverter;
     }
 
@@ -37,7 +34,7 @@ public class AccountService {
                 createAccountRequest.getInitialCredit(),
                 LocalDateTime.now());
         if (createAccountRequest.getInitialCredit().compareTo(BigDecimal.ZERO) > 0) {
-            Transaction transaction = transactionService.initiateMoney(account, createAccountRequest.getInitialCredit());
+            Transaction transaction =   new Transaction(createAccountRequest.getInitialCredit(),account);
             account.getTransaction().add(transaction);
         }
         return accountDtoConverter.convert(accountRepository.save(account));
