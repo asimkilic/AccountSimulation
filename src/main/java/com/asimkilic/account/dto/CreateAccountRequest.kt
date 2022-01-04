ea6cdf2ff26707a91dea3c19c599eef60c8e2b34
asimkilic <a.asim.kilic@gmail.com>
2022-01-04 20:35:00 +0300

    Validation added

--- src/main/java/com/asimkilic/account/dto/CreateAccountRequest.kt
+++ src/main/java/com/asimkilic/account/dto/CreateAccountRequest.kt
@@ -1,9 +1,11 @@
 package com.asimkilic.account.dto
 
 import java.math.BigDecimal
-
+import javax.validation.constraints.*
 data class CreateAccountRequest(
 
+        @field:NotBlank
         val customerId: String,
+        @field:Min(0)
         val initialCredit: BigDecimal
 )
